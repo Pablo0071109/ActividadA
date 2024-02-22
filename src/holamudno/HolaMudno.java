c1ca178bd2f8583dd88e56a54840235b11552b69
dam <dam@203P08>
2024-02-22 08:54:11 +0100

    Segundo commit (andres)

--- src/holamudno/HolaMudno.java
+++ src/holamudno/HolaMudno.java
@@ -16,6 +16,7 @@ public class HolaMudno {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Soy Pablo");
+        System.out.println("soy andres");
     }
     
 }
